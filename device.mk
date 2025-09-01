065a1e845d2c66491aec98ab8830f49d1b8b5a58
Rondeau79 <rondeau7@gmail.com>
2025-09-01 02:53:33 -0600

    pickup Evoultion strings

--- device.mk
+++ device.mk
@@ -16,7 +16,10 @@ PRODUCT_PACKAGES += \
     FrameworksResPstar \
     SystemUIResPstar \
     SettingsProviderResPstar \
-    $(LOCAL_PATH)/overlay-evolution
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-evolution 
 
 # Audio
 PRODUCT_COPY_FILES += \
